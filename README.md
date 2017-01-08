116936b324661cbf7f4315ffe191124a384b1d8d
midday <598535774@qq.com>
2017-01-08 21:31:44 +0800

    create project

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # fis3-deploy-gfe-combo-url
+fis3-deploy-gfe-combo-url
+
+
+## INSTALL
+
+```bash
+npm install [-g] fis3-deploy-gfe-combo-url
+```
+
+## USE
+```html
+	<!-- gfe:combo:begin -->
+    <link rel="stylesheet" href="<!--#include file='/n/common/default/style.html' -->">
+    <link rel="stylesheet" href="__CSS_DOMAIN__/gmlib/reset/1.0.0/reset.css">
+    <link rel="stylesheet" href="/css/common/common.css">
+    <!-- gfe:combo:end -->
+
+    <!--[if lte IE9]>
+    <link rel="stylesheet" href="/css/index/placeholder.css">
+    <![endif]-->
+
+    <!-- gfe:combo:begin -->
+    <link rel="stylesheet" href="/css/index/index.css">
+    <link rel="stylesheet" href="/css/index/test.css">
+    <!-- gfe:combo:end -->
+
+    <!-- gfe:combo:begin -->
+    <script src="<!--#include virtual='/n/common/a02/script.html' -->"></script>
+    <script src="__CSS_DOMAIN__/gmlib/unit/g/1.0.0/g.min.js"></script>
+    <script src="__CSS_DOMAIN__/gmlib/ui/gload/1.0.0/gload.min.js"></script>
+    <script src="/js/common/common.js"></script>
+    <!-- gfe:combo:end -->
+
+    <!--[if lte IE9]>
+    <script src="__CSS_DOMAIN__/gmlib/ui/gslider/1.0.4/gslider.min.js"></script>
+    <script src="__CSS_DOMAIN__/gmlib/ui/gslider/1.0.4/gslider.min.js"></script>
+    <![endif]-->
+
+    <!-- gfe:combo:begin -->
+    <script src="/js/index/index.js"></script>
+    <script src="/js/index/test.js"></script>
+    <!-- gfe:combo:end -->
+```
+
+```js
+fis.match('**', {
+    deploy: [
+        fis.plugin('gfe-combo-url'),//默认comboQuerySplit='??',comboQuerySplit=',',如有需要请自行扩展
+        fis.plugin('local-deliver') //must add a deliver, such as http-push, local-deliver
+    ]
+});
+```
---
+++ index.js
@@ -0,0 +1,89 @@
+'use strict';
+
+/**
+ * 对静态资源的url进行combo，请确保combo的资源包括在指定标签中
+ * @param  {Object}   options  插件配置
+ * @param  {Object}   modified 修改了的文件列表（对应watch功能）
+ * @param  {Object}   total    所有文件列表
+ * @param  {Function} next     调用下一个插件
+ * @return {undefined}
+ */
+module.exports = function(options, modified, total, next) {
+    //combo的url的分隔符
+    var comboUrlSplit = options.comboQuerySplit ? options.comboQuerySplit : '??';
+    //combo的查询分隔符
+    var comboQuerySplit = options.comboQuerySplit ? options.comboQuerySplit : ',';
+    //combo内容正则表达式
+    var comboRegExp = /<!--\s*gfe:combo:begin\s*-->((?!<!--\s*gfe:combo:end\s*-->)[\s\S])*<!--\s*gfe:combo:end\s*-->/gi;
+    //link标签正则表达式
+    var linkTagRegExp = /<link[^>]*?href\s*=\s*('[^']*'|"[^"]*")[^>]*?\/?>/gi;
+    //script标签正则表达式
+    var scriptTagRegExp = /<script[^>]*?src=('[^']*'|"[^"]*")[^>]*?>\s*<\s*\/script\s*>/gi;
+    //ssi标签正则表达式
+    var ssiTagRegExp = /<!--[ ]*#[ ]*([a-z]+)([ ]+([a-z]+)=("|')(.+?)("|'))*[ ]*-->/gi;
+    //domain正则表达式
+    var domainRegExp = /^((https:\/\/|http:\/\/|\/\/)[a-zA-Z\.0-9]+(?=\/))|(__CSS_DOMAIN__)|(__JS_DOMAIN__)/gi;
+    //combo的link标签
+    var comboLinkTag = '<link rel="stylesheet" href="COMBO_URL">';
+    //combo的script标签
+    var comboScriptTag = '<script src="COMBO_URL"></script>';
+
+    modified.forEach(function(file) {
+        if (file.isText() || typeof(file.getContent()) === 'string') {
+            var content = file.getContent();
+
+            //包含html基本结构的入口文件
+            var isEntryFile = (~content.indexOf('/html') || ~content.indexOf('/HTML')) && (~content.indexOf('/head') || ~content.indexOf('/HEAD')) && (~content.indexOf('/body') || ~content.indexOf('/BODY'));
+
+            //html入口文件(非-debug调试文件)才combo
+            if (/\.(html|ftl)/.test(file.rExt) && !/-debug$/.test(file.filename) && isEntryFile) {
+                
+
+                content = content.replace(comboRegExp, function(comboContent) {
+                    var comboTag = '';
+                    var domain = '';
+                    var pathArray = [];
+                    var urlArray = [];
+
+                    //link标签combo
+                    if (linkTagRegExp.test(comboContent)) {
+                        comboTag = comboLinkTag;
+                        urlArray = comboContent.match(linkTagRegExp).map(function(value) {
+                            //提取url并去除url首尾引号
+                            return value.replace(linkTagRegExp, '$1').slice(1, -1);
+                        });
+                    }
+                    //script标签combo
+                    if (scriptTagRegExp.test(comboContent)) {
+                        comboTag = comboScriptTag;
+                        urlArray = comboContent.match(scriptTagRegExp).map(function(value) {
+                            //提取url并去除url首尾引号
+                            return value.replace(scriptTagRegExp, '$1').slice(1, -1);
+                        });
+                    }
+
+                    urlArray.forEach(function(url, index) {
+                        if (index === 0 && !ssiTagRegExp.test(url)) {
+                            //以常量，http,https,//开头的域名
+                            if (domainRegExp.test(url)) {
+                                domain = url.match(domainRegExp)[0];
+                            }
+                        }
+
+                        var path = url.replace(domainRegExp, '').replace(/^\//, '');
+                        pathArray.push(path);
+                    });
+
+                    //如果domain为空，说明ssi为第一个，则不添加域名
+                    var comboUrl = domain + (domain !== '' ? comboUrlSplit : '') + pathArray.join(comboQuerySplit);
+                    comboTag = comboTag.replace('COMBO_URL', comboUrl);
+
+                    return comboTag;
+                });
+
+                file.setContent(content);
+            }
+        }
+    });
+    next();
+};
